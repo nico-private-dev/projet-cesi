6ade6110624bc3cad53ba6d1d7e032d8d5b1bc15
FORGET Nicolas <nforget82@gmail.com>
2024-03-20 16:08:45 +0100

    maj

---
+++ view/_parts/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">QrFim</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">à propos
+                        </a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <button>Me connecter</button>
+                    <button>Créer un compte</button>
+                </div>
+            </div>
+        </div>
+    </nav>
