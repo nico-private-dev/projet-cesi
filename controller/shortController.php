9a27fdf5c65aa0dc37444905f777344e4b58c0d5
FERTRE Grégoire <gregoire.fertre@gmail.com>
2024-03-20 15:12:52 +0100

    MAJ BDD + Function

--- controller/shortController.php
+++ controller/shortController.php
@@ -1,20 +1,32 @@
 <?php 
 
 function shortenUrl($url) {
-    // Génère un identifiant unique et court pour l'URL
+
     $shortId = uniqid();
 
-    // Ici, dans une application réelle, vous stockeriez $shortId et $url dans une base de données
-    // pour pouvoir retrouver l'URL à partir de $shortId
-    
-    // Construit une URL courte en utilisant $shortId (exemplatif)
+
     $shortUrl = "https://qrfim.xyz/" . $shortId;
     
-    // Assurez-vous que la longueur ne dépasse pas 50 caractères
     if (strlen($shortUrl) > 50) {
-        // Traitez l'erreur ou ajustez $shortId pour réduire la longueur
+       
         return "Erreur: L'URL raccourcie dépasse 50 caractères.";
     }
     
     return $shortUrl;
+}
+
+function addUrls($url_full, $user_id){
+
+    $date = new DateTime();
+
+    $req = "INSERT INTO urls (url_full, user_id, alikes, date_created) VALUES (:url_full, :user_id, 0, '". $date->format("Y-m-d h:i:s")."');";
+
+    $data = [
+        'url_full' => $url_full,
+        'user_id' => $user_id
+    ];
+
+    databaseWrite($req, $data);
+
+
 }
