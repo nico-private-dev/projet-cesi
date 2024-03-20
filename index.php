c4fe008022f8afc8631fc0e4739ef9284609b00a
FORGET Nicolas <nforget82@gmail.com>
2024-03-20 15:20:08 +0100

    maj index.php

--- index.php
+++ index.php
@@ -16,3 +16,28 @@ $shortUrl = shortenUrl($longUrl);
 echo $shortUrl;
 require_once "./view/_parts/footer.php";
 
+$page = "home";
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+switch ($page) {
+    case 'home':
+        require_once "./view/home.php";
+
+        break;
+    case 'admin':
+
+
+        break;
+    case 'action-add-post':
+        require_once "./view/actions/add_post.php";
+
+        break;
+
+    default:
+        http_response_code(404);
+        require_once "./view/404.php";
+
+        break;
+}
---
+++ view/home.php
@@ -0,0 +1,13 @@
+<form>
+    <fieldset>
+        <legend>Legend</legend>
+        <div class="row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com" data-keeper-lock-id="k-gmjvc7q802m">
+                <keeper-lock class="focus-visible logged-in" tabindex="0" id="k-gmjvc7q802m" aria-label="Open Keeper Popup" role="button" style="background-image: url(&quot;chrome-extension://bfogiafebfohielmmehodmfbbebbbpei/images/ico-field-fill-lock.svg&quot;) !important; background-size: 24px 24px !important; cursor: pointer !important; width: 24px !important; position: absolute !important; opacity: 1 !important; margin-top: auto !important; min-width: 24px !important; top: 51px; left: 609px; z-index: 1; padding: 0px; animation-name: none; height: 24px !important;"></keeper-lock>
+            </div>
+        </div>
+
+    </fieldset>
+</form>
