7cdf8cdca68278902198612fd76965c72acb296d
FORGET Nicolas <nforget82@gmail.com>
2024-03-21 15:29:07 +0100

    commit

--- controller/formController.php
+++ controller/formController.php
@@ -4,7 +4,8 @@ function addUrl($url_full, $url_short){
 
     $date = new DateTime();
 
-    $req = "INSERT INTO urls (url_full, url_short, limit_date, created_at, updated_at) VALUES (:url_full, :url_short, null, '". $date->format("Y-m-d h:i:s")."', null);";
+    $req = "INSERT INTO urls (user_id, url_full, url_short, limit_date, created_at, updated_at) VALUES (1, :url_full, :url_short, null, '". $date->format("Y-m-d h:i:s")."', null);
+    ;";
 
     $data = [
         'url_full' => $url_full,
