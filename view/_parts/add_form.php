ed92029c08936673e876255c505c52cbb935875c
FORGET Nicolas <nforget82@gmail.com>
2024-03-21 14:34:07 +0100

    commit

--- view/_parts/add_form.php
+++ view/_parts/add_form.php
@@ -1,6 +1,8 @@
-<form action="#" method="post">
-    <div class="row">
-        <input type="text" name="url-full" id="url-full">
-        <input type="submit" value="Générer">
+<form action="#" method="post" class="container col-6">
+    <div class="row d-flex justify-content-center">
+        <div class="d-flex justify-content-center">
+            <input type="text" name="url-full" id="url-full">
+            <input type="submit" value="Générer">
+        </div>
     </div>
 </form>
