4860cb1cc1a8d83de9a482bc9e892df64a9c78f3
FORGET Nicolas <nforget82@gmail.com>
2024-03-21 14:53:25 +0100

    commit

--- controller/formController.php
+++ controller/formController.php
@@ -1,14 +1,14 @@
 <?php
 
-function addUrl($content, $user_id){
+function addUrl($url_full, $url_short){
 
     $date = new DateTime();
 
-    $req = "INSERT INTO blog (content, user_id, alikes, date_created) VALUES (:content, :user_id, 0, '". $date->format("Y-m-d h:i:s")."');";
+    $req = "INSERT INTO urls (url_full, url_short, limit_date, created_at, updated_at) VALUES (:url_full, :url_short, null, '". $date->format("Y-m-d h:i:s")."', null);";
 
     $data = [
-        'content' => $content,
-        'user_id' => $user_id
+        'url_full' => $url_full,
+        'url_short' => $url_short
     ];
 
     databaseWrite($req, $data);
