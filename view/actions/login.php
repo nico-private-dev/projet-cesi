b9158830d4e45d03d52fc1efd61ecda1db622304
paul <paul.biret18@gmail.com>
2024-03-25 15:08:23 +0100

    c'est paul 2

---
+++ view/actions/login.php
@@ -0,0 +1,24 @@
+<?php 
+
+// Je controle que tpout les champs sont rempli et qte c'est bien une requete de type POST
+if(isset($_POST) && isset($_POST['login']) && isset($_POST['password'])) {
+
+    $login = cleanStr($_POST['login']);
+    $password = cleanStr($_POST['password']);
+
+    $user_id = login($login, $password);
+
+    if($user_id) {
+        $user = getUserById($user_id);
+        $_SESSION['user']['nickname'] = $user['nickname'];
+        addFlash("success", "Bon retour par minou (😺) " . $_SESSION['user']['nickname']);
+        header("location:?page=home");
+    } else {
+        // user = 0
+        // donc message compte inexistant
+        addFlash("danger", "Erreur, utilisateur inconnu");
+        header("location:?page=login");
+    }
+
+    
+}
