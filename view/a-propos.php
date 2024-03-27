b699230160e38aa899f4ea61c9f62768c06b9e2f
FORGET Nicolas <nforget82@gmail.com>
2024-03-27 16:35:45 +0100

    commit page a propos

---
+++ view/a-propos.php
@@ -0,0 +1,36 @@
+<div class="mt-5 container">
+    <div class="row">
+        <h1>À propos de Qr FIM</h1>
+        <p>Qr FIM est une initiative conçue et développée par les étudiants et pour les étudiants de [Nom de l'École]. Notre mission est de simplifier le partage d'informations au sein de notre communauté éducative en utilisant la technologie des QR codes.</p>
+        <br>
+        <p>Dans notre quête d'innovation et d'efficacité, nous avons remarqué que les liens vers des ressources éducatives, des événements et des annonces importantes pouvaient parfois être longs et complexes. [Nom de l'Application] résout ce problème en offrant un moyen rapide et facile de créer des QR codes pour des URLs raccourcies, spécialement conçues pour notre école. Ces codes peuvent être scannés en un instant, menant directement aux ressources partagées sans le tracas de taper des adresses web longues et compliquées.</p>
+        <br>
+
+        <h2>Notre Vision</h2>
+        <br>
+        <p>Nous croyons en une école connectée où l'accès à l'information est instantané et sans barrière. [Nom de l'Application] est plus qu'un simple outil de création de QR codes ; c'est une passerelle vers une communication plus fluide et une meilleure accessibilité aux ressources précieuses qui nourrissent notre apprentissage quotidien.</p>
+        <br>
+
+        <h2>Fonctionnaités</h2>
+        <br>
+        <ul>
+            <li><strong>Création facile de QR codes</strong> : Générez en quelques clics des QR codes pour n'importe quel lien lié à l'école.</li>
+            <li><strong>Accessibilité </strong> : Facilitez l'accès aux ressources éducatives, aux formulaires d'inscription, aux sites d'événements et plus encore, pour toute personne possédant un smartphone.</li>
+        </ul>
+        <br>
+
+        <h2>Pourquoi utiliser Qr FIM?</h2>
+        <ul>
+            <li>
+                <strong>Rapidité</strong>: Fini le temps perdu à saisir des URLs longues et compliquées. Un simple scan et vous y êtes.
+            </li>
+            <li>
+                <strong>Simplicité </strong>: Interface utilisateur intuitive conçue pour tous, que vous soyez technophile ou non
+            </li>
+            <li>
+                <strong>Engagement  </strong>: Renforcez l'engagement de la communauté éducative en rendant l'information facilement accessible.
+            </li>
+        </ul>
+
+    </div>
+</div>
